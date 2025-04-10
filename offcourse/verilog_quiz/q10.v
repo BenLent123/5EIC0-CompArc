4aabacfb5a4afbde9693fcaa7175c8cc0ca19300
Ben Lent <134686333+BenLent123@users.noreply.github.com>
2025-04-10 17:46:15 +0200

    Create q10.v

---
+++ offcourse/verilog_quiz/q10.v
@@ -0,0 +1,20 @@
+module	UUT (
+		input clk,
+		input enable,
+		input in,
+		output out
+	);
+
+	reg [7:0] register;
+
+	always @(posedge clk) begin
+		if (enable) begin
+			register = {register[6:0], in};
+		end else begin
+			register <= register;
+		end
+	end
+
+	assign out = register[7];
+
+endmodule
